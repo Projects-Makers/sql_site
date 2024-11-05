4666cf7fcad24108d48fe5c22b4ca000a06fd539
gnatjaku <jakub.gnat@gmail.com>
2024-11-05 11:20:23 +0100

    php
    dodane includy

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+include 'index.html';
+?>
+    <?php 
+    include("baner.html");
+    include("glowna.html");
+    include("stopka.html");
+    //if (!isset($_GET["strona"]))
+    //    {$plik = '';}
+    //else
+    //  {$plik = $_GET["strona"];}
+    //$roz = '.php';
+    //include("$plik$roz");
+    ?>
