991d6bd6165ee68290a058e63ff430f289730f35
Oskar Jabłoński <jablonskio@tlimc.local>
2024-11-06 10:06:34 +0100

    e

---
+++ html/javascript/login.js
@@ -0,0 +1,7 @@
+        function toggleMenu() {
+            const menu = document.getElementById('menu');
+            const hamburger = document.getElementById('hamburger');
+            menu.classList.toggle('open');
+            hamburger.classList.toggle('active'); // Poprawione z kropką
+        }
+s
